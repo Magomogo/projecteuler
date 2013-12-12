0f60dd0d9bc7ae5deece9ea755a3e7dc47e7c43e
Magomogo <mgnatenko@gmail.com>
2013-12-12 08:32:55 +0700

    shorten

--- problem4.js
+++ problem4.js
@@ -1,20 +1,15 @@
 var strRev = function (s) {
-        var rev = [];
-        (s + '').split('').map(function (c) { rev.unshift(c);});
-        return rev.join('');
+        return (s + '').split('').reverse().join('');
     },
     isPalindromic = function (num) {
-        var string = num + '';
-        return string.substr(0, Math.ceil(string.length / 2)) === strRev(string.substr(Math.floor(string.length / 2)));
+        return num + '' === strRev(num + '');
     },
     result = 0;
 
 for (var x = 100; x <= 999; x++) {
     for (var y = 100; y <= x; y++) {
         if (isPalindromic(x * y)) {
-            if (result < x * y) {
-                result = x * y;
-            }
+            result = Math.max(x * y, result);
         }
     }
 }
